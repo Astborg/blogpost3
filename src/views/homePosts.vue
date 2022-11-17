48ea6e556697fb2256dc170753b6b2a76cc3c34b
Astborg <josefinastborg@hotmail.com>
2022-11-17 11:20:36 +0100

    tailwind changes2

--- src/views/homePosts.vue
+++ src/views/homePosts.vue
@@ -1,13 +1,16 @@
 <template>
+    <div v-if="showModal" @click="showModal = false" class="absolute w-screen h-screen bg-black opacity-75 z-10 flex cursor-pointer items-center justify-center">
+        <div class="bg-white p-4 rounded-full">Please write a new blogpost! Click here or write a post!</div>
+    </div>
 <div class="w-6/12 text-center m-auto">
     <h1 class="text-4xl font-bold text-indigo-500 m-5">Blog Posts</h1>
-    <div class="border-2 border-slate-100 rounded-3xl hover:border-red-400" v-for="note in notes" :key="note.id" > 
+    <div  class="border-2 border-slate-100 rounded-3xl hover:border-red-400" v-for="note in notes" :key="note.id" > 
         <div class="relative flex flex-col m-0 justify-around cursor-pointer">
             <button class="absolute top-0 right-0 w-5 h-5 cursor-pointer rounded-full hover:bg-red-400 p-2.5" @click="deleteBtn(note)">x</button>
             <img class="w-24 h-24 p-3.5 rounded-full" :src="note.img"> 
             <p class="text-xs text-right pr-3.5"> {{ note.writer }}</p>
             <p class="text-lg font-bold"> {{ note.headline }}</p>
-            <p class="text-left p-3.5 text-sm"> {{ note.text }}</p>
+            <p @click="showModal = true" class="text-left p-3.5 text-sm"> {{ note.text }}</p>
         </div>
     </div>
 </div>
@@ -29,5 +32,9 @@ notes.value = JSON.parse(localStorage.getItem('notes'));
         console.log(notes.value)
     }
 
+const showModal = ref(false)
 </script>
+<style>
+
+</style>
 
